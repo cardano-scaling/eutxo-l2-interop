e6df612063b6cd5dd58f16f27daddbe94defed2f
Valentino Cerutti <valentinocerutti@gmail.com>
2025-10-16 21:31:32 -0300

    Adds getUserDetails function to utils module

--- src/offchain/lib/utils.ts
+++ src/offchain/lib/utils.ts
@@ -1,4 +1,7 @@
-import { LucidEvolution, Network } from "@lucid-evolution/lucid";
+import { CML, LucidEvolution, Network } from "@lucid-evolution/lucid";
+import { readFileSync } from "node:fs";
+import { join } from "node:path";
+import { type Interface } from 'node:readline/promises';
 
 function getNetworkFromLucid(lucid: LucidEvolution): Network {
     const network = lucid.config().network;
@@ -8,4 +11,67 @@ function getNetworkFromLucid(lucid: LucidEvolution): Network {
     return network;
   }
 
-export { getNetworkFromLucid }
+type UserDetails = {
+    sk: CML.PrivateKey;
+    vk: CML.PublicKey;
+    senderNodeUrl?: string;
+    receiverNodeUrl?: string;
+}
+
+async function getUserDetails(role: string, rli: Interface): Promise<UserDetails> {
+
+    const ASK_FOR_NAME = `Select which user will be the ${role}:
+    1. Alice
+    2. Bob
+    3. Ida\n`;
+
+    let name = ""
+    let senderNodeUrl = undefined
+    let receiverNodeUrl = undefined
+
+    while (name === "") {
+
+        const index = await rli.question(ASK_FOR_NAME);
+
+        switch(index) {
+            case '1':
+                name = "alice"
+                senderNodeUrl = "http://127.0.0.1:4001"
+                break
+            case `2`:
+                name = "bob"
+                receiverNodeUrl = "http://127.0.0.1:4002"
+                break
+            case '3':
+                name = "ida"
+                senderNodeUrl = "http://127.0.0.1:4004"
+                receiverNodeUrl = "http://127.0.0.1:4003"
+                break
+            default:
+                rli.write(`Invalid choice ${index}`)
+        }
+    }
+
+    const skPath = join(process.cwd(), `../infra/credentials/${name}/${name}-funds.sk`);
+    const sk = JSON.parse(readFileSync(skPath, 'utf8'));
+
+    const skBytes = Buffer.from(sk.cborHex, 'hex');
+    const cmlSk = CML.PrivateKey.from_normal_bytes(skBytes.subarray(2));
+
+    const vkPath = join(process.cwd(), `../infra/credentials/${name}/${name}-funds.vk`);
+    const vk = JSON.parse(readFileSync(vkPath, 'utf8'));
+
+    const vkBytes = Buffer.from(vk.cborHex, 'hex');
+    const cmlVk = CML.PublicKey.from_bytes(vkBytes.subarray(2));
+
+    const userDetails: UserDetails = {
+        sk: cmlSk,
+        vk: cmlVk,
+        senderNodeUrl,
+        receiverNodeUrl,
+    }
+
+    return userDetails
+}
+
+export { getNetworkFromLucid, getUserDetails }
