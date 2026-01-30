03a979a599e8d0ce4ee656e09dc988d08c65d0c7
Franco Luque <francolq@gmail.com>
2026-01-30 14:41:46 -0300

    refactor wrap test to have reusable functions

--- src/onchain/tests/wrap.ts
+++ src/onchain/tests/wrap.ts
@@ -5,110 +5,121 @@ import {
   CardanoTransactionOutputReference
 } from "./plutus.ts";
 
-// ALICE
-const privateKey1 = Crypto.generatePrivateKey();
-const address1 = Addresses.credentialToAddress(
-  { Emulator: 0 },
-  Crypto.privateKeyToDetails(privateKey1).credential,
-);
-const alice = {
-  address: address1,
-  assets: { lovelace: 3000000000n },
-};
+// Export reusable setup function
+export function setupWrapEnvironment() {
+  // ALICE
+  const privateKey1 = Crypto.generatePrivateKey();
+  const address1 = Addresses.credentialToAddress(
+    { Emulator: 0 },
+    Crypto.privateKeyToDetails(privateKey1).credential,
+  );
+  const alice = {
+    address: address1,
+    assets: { lovelace: 3000000000n },
+  };
 
-// IDA
-const privateKey2 = Crypto.generatePrivateKey();
-const address2 = Addresses.credentialToAddress(
-  { Emulator: 0 },
-  Crypto.privateKeyToDetails(privateKey2).credential,
-);
-const ida = {
-  address: address2,
-  assets: { lovelace: 3000000000n },
-};
+  // IDA
+  const privateKey2 = Crypto.generatePrivateKey();
+  const address2 = Addresses.credentialToAddress(
+    { Emulator: 0 },
+    Crypto.privateKeyToDetails(privateKey2).credential,
+  );
+  const ida = {
+    address: address2,
+    assets: { lovelace: 3000000000n },
+  };
 
-// BOB
-const privateKey3 = Crypto.generatePrivateKey();
-const address3 = Addresses.credentialToAddress(
-  { Emulator: 0 },
-  Crypto.privateKeyToDetails(privateKey3).credential,
-);
-const bob = {
-  address: address3,
-  assets: { lovelace: 3000000000n },
-};
+  // BOB
+  const privateKey3 = Crypto.generatePrivateKey();
+  const address3 = Addresses.credentialToAddress(
+    { Emulator: 0 },
+    Crypto.privateKeyToDetails(privateKey3).credential,
+  );
+  const bob = {
+    address: address3,
+    assets: { lovelace: 3000000000n },
+  };
 
-// SETUP EMULATORS FOR HYDRA HEADS A AND B
-const emulatorA = new Emulator([alice, ida]);
-const emulatorB = new Emulator([ida, bob]);
+  // SETUP EMULATORS FOR HYDRA HEADS A AND B
+  const emulatorA = new Emulator([alice, ida]);
+  const emulatorB = new Emulator([ida, bob]);
 
-// ALICE'S LUCID INSTANCE IN HEAD A
-const lucid1A = new Lucid({
-  provider: emulatorA,
-  wallet: { PrivateKey: privateKey1 },
-});
+  // ALICE'S LUCID INSTANCE IN HEAD A
+  const lucid1A = new Lucid({
+    provider: emulatorA,
+    wallet: { PrivateKey: privateKey1 },
+  });
 
-// IDA'S LUCID INSTANCE IN HEAD B
-const lucid2B = new Lucid({
-  provider: emulatorB,
-  wallet: { PrivateKey: privateKey2 },
-});
+  // IDA'S LUCID INSTANCE IN HEAD B
+  const lucid2B = new Lucid({
+    provider: emulatorB,
+    wallet: { PrivateKey: privateKey2 },
+  });
 
-//
-// WRAP UTXO IN A: Alice wraps 5 ADA in head A
-//
-const wrappedValidator = new AdhocLedgerV4WrappedSpend();
-const wrappedAddress = lucid1A.newScript(wrappedValidator).toAddress();
-const wrappedDatum: AdhocLedgerV4WrappedDatum = {
-  owner: "",           // TODO: Alice address here!
-  intermediaries: [],  // TODO: Ida address here!
+  return {
+    privateKey1, privateKey2, privateKey3,
+    address1, address2, address3,
+    alice, ida, bob,
+    emulatorA, emulatorB,
+    lucid1A, lucid2B
+  };
 }
-const wrapTxA = await lucid1A.newTx()
-  .payToContract(
-    wrappedAddress,
-          { Inline: Data.to({
-        owner: "",           // TODO: Alice address here!
-        intermediaries: [],  // TODO: Ida address here!
-      },
-  AdhocLedgerV4WrappedSpend.datumOpt) },
-    { lovelace: 5000000n },
-  )
-  .commit();
-// console.log("TX:", wrapTxA.toString());
-const signedWrapTxA = await wrapTxA.sign().commit();
-const wrapTxAHash = await signedWrapTxA.submit();
-// console.log("WRAP TX A:", signedWrapTxA.toString());
-emulatorA.awaitTx(wrapTxAHash);
-console.log("WRAP TX A:", wrapTxAHash);
 
-//
-// WRAP UTXO IN B: Ida wraps 5 ADA in head B on behalf of Alice
-//
-const wrappedAddressB = lucid2B.newScript(wrappedValidator).toAddress();
-const wrappedDatumB: AdhocLedgerV4WrappedDatum = {
-  owner: "",           // TODO: Alice address here!
-  intermediaries: [],  // TODO: Ida address here!
+// Export reusable wrap transaction function
+export async function performWrapTransactions(lucid1A: Lucid, lucid2B: Lucid, privateKey1: any, privateKey2: any, emulatorA: Emulator, emulatorB: Emulator) {
+  const wrappedValidator = new AdhocLedgerV4WrappedSpend();
+  const wrappedAddress = lucid1A.newScript(wrappedValidator).toAddress();
+
+  // WRAP UTXO IN A: Alice wraps 5 ADA in head A
+  const wrappedDatum: AdhocLedgerV4WrappedDatum = {
+    owner: Crypto.privateKeyToDetails(privateKey1).credential.hash,           // Alice address hash
+    intermediaries: [Crypto.privateKeyToDetails(privateKey2).credential.hash],  // Ida address hash
+  }
+  const wrapTxA = await lucid1A.newTx()
+    .payToContract(
+      wrappedAddress,
+      { Inline: Data.to(wrappedDatum, AdhocLedgerV4WrappedSpend.datumOpt) },
+      { lovelace: 5000000n },
+    )
+    .commit();
+  const signedWrapTxA = await wrapTxA.sign().commit();
+  const wrapTxAHash = await signedWrapTxA.submit();
+  emulatorA.awaitTx(wrapTxAHash);
+  console.log("WRAP TX A:", wrapTxAHash);
+
+  // WRAP UTXO IN B: Ida wraps 5 ADA in head B on behalf of Alice
+  const wrappedDatumB: AdhocLedgerV4WrappedDatum = {
+    owner: Crypto.privateKeyToDetails(privateKey1).credential.hash,           // Alice address hash
+    intermediaries: [Crypto.privateKeyToDetails(privateKey2).credential.hash],  // Ida address hash
+  }
+  const wrapTxB = await lucid2B.newTx()
+    .payToContract(
+      wrappedAddress,
+      { Inline: Data.to(wrappedDatumB, AdhocLedgerV4WrappedSpend.datumOpt) },
+      { lovelace: 5000000n },
+    )
+    .commit();
+  const signedWrapTxB = await wrapTxB.sign().commit();
+  const wrapTxBHash = await signedWrapTxB.submit();
+  emulatorB.awaitTx(wrapTxBHash);
+  console.log("WRAP TX B:", wrapTxBHash);
+
+  return { wrappedValidator, wrappedAddress, wrappedDatum };
 }
-const wrapTxB = await lucid2B.newTx()
-  .payToContract(
-    wrappedAddress,
-    { Inline: Data.to(wrappedDatumB, AdhocLedgerV4WrappedSpend.datumOpt) },
-    { lovelace: 5000000n },
-  )
-  .commit();
-// console.log("TX:", wrapTxB.toString());
-const signedWrapTxB = await wrapTxB.sign().commit();
-const wrapTxBHash = await signedWrapTxB.submit();
-// console.log("WRAP TX B:", signedWrapTxB.toString());
-emulatorB.awaitTx(wrapTxBHash);
-console.log("WRAP TX B:", wrapTxBHash);
 
+// Check if this file is being executed directly or imported
+if (import.meta.main) {
+  // This file is being executed directly (e.g., deno run wrap.ts)
+  console.log("Running wrap.ts as main module...");
 
-// 
-// VERIFY:
-// 
+  // Setup environment
+  const env = setupWrapEnvironment();
 
+  // Perform wrap transactions
+  await performWrapTransactions(env.lucid1A, env.lucid2B, env.privateKey1, env.privateKey2, env.emulatorA, env.emulatorB);
 
-// 
-// PERFORM:
-// 
+  console.log("Wrap transactions completed successfully!");
+} else {
+  // This file is being imported by another module
+  console.log("wrap.ts imported as module");
+}
